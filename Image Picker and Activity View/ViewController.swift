10a0f5d63b49a75085551902454f7dccd131746c
Abdelrahman Mohamed <obada.semary@gmail.com>
2015-12-09 16:27:14 +0300

    1.1

--- Image Picker and Activity View/ViewController.swift
+++ Image Picker and Activity View/ViewController.swift
@@ -20,6 +20,35 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func experiment(sender: UIButton) {
+    
+        let nextController = UIImagePickerController()
+        
+        self.presentViewController(nextController, animated: true, completion: nil)
+    }
+    
+    @IBAction func ImageExperiment(sender: UIButton) {
+        
+        let image = UIImage()
+        let nextController = UIActivityViewController(activityItems: [image], applicationActivities: nil)
+        
+        self.presentViewController(nextController, animated: true, completion: nil)
+    }
+    
+    @IBAction func alertExperiment(sender: UIButton) {
+        
+        let nextController = UIAlertController()
+        
+        nextController.title = "Test alert"
+        nextController.message = "This is a test Alert 😂"
+        
+        let okAction = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default){
+            action in self.dismissViewControllerAnimated(true, completion: nil)
+        }
+        
+        nextController.addAction(okAction)
+        
+        self.presentViewController(nextController, animated: true, completion: nil)
+    }
 }
 
